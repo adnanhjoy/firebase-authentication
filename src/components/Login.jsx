3f9e90feab352fc549305b9211198d8463755ad2
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-21 11:28:47 +0600

    added github sign in

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import {GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
 import app from '../firebase/firebase.init';
 
 const auth = getAuth(app)
 
 const Login = () => {
     const googlePorvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
 
 const googleSignIn = () => {
     signInWithPopup(auth, googlePorvider)
+    .then(result => {
+        const user = result.user;
+        console.log(user)
+    })
+    .catch(error => {
+        console.error(error)
+    })
+}
+
+const githubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
     .then(result => {
         const user = result.user;
         console.log(user)
@@ -35,10 +47,10 @@ const googleSignIn = () => {
 
                 <p className='text-white'>Don't have an account ? Please <Link className='text-black' to='/register'>Signup</Link></p>
 
-                <div className='mt-5 flex justify-center flex-col items-center'>
-                    <h1 onClick={googleSignIn} className='bg-white w-1/3 rounded-md p-2'>Sign in with google</h1>
-                    <h1 className='bg-white w-1/3 rounded-md p-2 my-2'>Sign in with faceook</h1>
-                    <h1 className='bg-white w-1/3 rounded-md p-2'>Sign in with github</h1>
+                <div className=' text-center mt-5 flex justify-center flex-col items-center'>
+                    <h1 onClick={googleSignIn} className='bg-white w-1/3 rounded-md p-2 cursor-pointer'>Sign in with google</h1>
+                    <h1  className='bg-white w-1/3 rounded-md p-2 my-2 cursor-pointer'>Sign in with faceook</h1>
+                    <h1 onClick={githubSignIn} className='bg-white w-1/3 rounded-md p-2 cursor-pointer'>Sign in with github</h1>
                 </div>
             </form>
         </div>
