8e29a03222749bb91b9ba27304eb007b345c03a5
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-21 10:15:35 +0600

    push react boiler templete

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDzn94OeCOi7cd9JFBdd4TDBNzERm5iDWk",
+  authDomain: "fir-authentication-2b9e8.firebaseapp.com",
+  projectId: "fir-authentication-2b9e8",
+  storageBucket: "fir-authentication-2b9e8.appspot.com",
+  messagingSenderId: "737842523604",
+  appId: "1:737842523604:web:86fbe16784951833b13a41"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
