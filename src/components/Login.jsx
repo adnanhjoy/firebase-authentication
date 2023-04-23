a49ef3a206e78453a9188c58984f03b09987a884
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-23 09:06:42 +0600

    added login and forgotten step

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, getAuth, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 import app from '../firebase/firebase.init';
 
 const auth = getAuth(app)
 
 const Login = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+    const [success, setSuccess] = useState(false);
+    const [userEmail, setUserEmail] = useState('')
+
+
     const googlePorvider = new GoogleAuthProvider()
     const githubProvider = new GithubAuthProvider()
     const facebookProvider = new FacebookAuthProvider()
 
+    const loginUsers = (event) => {
+        event.preventDefault()
+        setSuccess(false)
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                result.user;
+                form.reset()
+                setSuccess(true)
+            })
+            .catch(error => {
+                console.error(error)
+                setErrorMessage(error);
+            })
+    }
+
+    const getResetEmail = event => {
+        setUserEmail(event.target.value)
+    }
+
+
+    const forgottenPassword = () => {
+        sendPasswordResetEmail(auth, userEmail)
+            .then(() => {
+                alert('Please check your email and reset your email')
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
     const googleSignIn = () => {
         signInWithPopup(auth, googlePorvider)
             .then(result => {
-                const user = result.user;
-                console.log(user)
+                result.user;
             })
             .catch(error => {
                 console.error(error)
@@ -24,8 +63,7 @@ const Login = () => {
     const githubSignIn = () => {
         signInWithPopup(auth, githubProvider)
             .then(result => {
-                const user = result.user;
-                console.log(user)
+                result.user;
             })
             .catch(error => {
                 console.error(error)
@@ -35,8 +73,7 @@ const Login = () => {
     const facebookSignIn = () => {
         signInWithPopup(auth, facebookProvider)
             .then(result => {
-                const user = result.user;
-                console.log(user)
+                result.user;
             })
             .catch(error => {
                 console.error(error)
@@ -46,16 +83,20 @@ const Login = () => {
     return (
         <div className=' bg-indigo-600 p-20'>
             <h1 className='text-white text-center text-5xl'>Please Login</h1>
-            <form className='w-1/2 mx-auto'>
+            <form onSubmit={loginUsers} className='w-1/2 mx-auto'>
                 <div className='my-5 flex flex-col'>
                     <label className='text-white mb-3' htmlFor="">Enter your email</label>
-                    <input className=' w-full p-2 rounded-md' type="text" name="" id="" />
+                    <input onBlur={getResetEmail} className=' w-full p-2 rounded-md' type="email" name="email" id="" />
                 </div>
                 <div className=' flex flex-col'>
                     <label className='text-white mb-3' htmlFor="">Enter your password</label>
-                    <input className=' w-full p-2 rounded-md' type="text" name="" id="" />
+                    <input className=' w-full p-2 rounded-md' type="password" name="password" id="" />
                 </div>
+                {success && <p>Login Succesfull</p>}
+                <p>{errorMessage}</p>
                 <button className=' bg-violet-500 py-3 px-5 rounded-md text-white mt-5 hover:bg-black'>Login</button>
+
+                <p onClick={forgottenPassword}><Link className='text-white hover:underline hover:text-orange-500'>Forgotten Password ?</Link></p>
 
                 <p className='text-white'>Don't have an account ? Please <Link className='text-black' to='/register'>Signup</Link></p>
 
